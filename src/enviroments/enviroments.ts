7775998734bdab201365881bb808ca8b7d888064
celeste <cococete.07@gmail.com>
2023-04-26 08:55:05 -0500

    primera copia

---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+    production: false, 
+    firebaseConfig:{
+       apiKey: "AIzaSyCV6WRM3IYWS1RmmnSvNuy2GuR8OAm51Dw",
+       authDomain: "celenews-fbaf0.firebaseapp.com",
+       projectId: "celenews-fbaf0",
+       storageBucket: "celenews-fbaf0.appspot.com",
+       messagingSenderId: "383701524827",
+       appId: "1:383701524827:web:a91f0154a50b58c536ba7e"
+      }
+  };
+  
